2b1b47f2652573d9db1d4b6f0aca8f7aa62277a7
Sanjay Paraboo <paraboosanjay@gmail.com>
2017-09-19 15:06:18 -0400

    Started Lab A. LabA1 Done

---
+++ src/LabA/LabA1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Sanjay on 9/18/2017.
+ */
+public class LabA1 {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(Integer.parseInt(args[0])));
+    }
+}
---
+++ src/LabA/LabA2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Sanjay on 9/19/2017.
+ */
+public class LabA2 {
+    public LabA2() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toHexString(Integer.parseInt(args[0])));
+    }
+}
