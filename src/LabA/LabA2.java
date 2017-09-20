9df20a6e56bf4b91ecb218aeb4c56b6b261c2c27
Sanjay Paraboo <paraboosanjay@gmail.com>
2017-09-19 20:22:24 -0400

    Finished LabA2. Started LabA3.

--- src/LabA/LabA2.java
+++ src/LabA/LabA2.java
@@ -6,6 +6,10 @@ public class LabA2 {
     }
 
     public static void main(String[] args) {
-        System.out.println(Integer.toHexString(Integer.parseInt(args[0])));
+        System.out.println("0x" + Integer.toHexString(Integer.parseInt(args[0])));
+        System.out.println("0x" + Integer.toHexString(Integer.reverse(Integer.parseInt(args[0]))));
+        System.out.println("0x" + Integer.toHexString(Integer.reverseBytes(Integer.parseInt(args[0]))));
     }
 }
+
+
---
+++ src/LabA/LabA3.java
@@ -0,0 +1,16 @@
+
+/**
+ * Created by Sanjay on 9/19/2017.
+ */
+public class LabA3 {
+    public LabA3() {
+    }
+
+    public static void main(String[] args) {
+        Integer x = Integer.parseInt(args[0]);
+        Integer y = Integer.parseInt(args[1]);
+
+        System.out.printf
+    }
+}
+
