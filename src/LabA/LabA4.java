64cb62f7a6bf789e8ac26b34cad5cda526fdb783
Sanjay Paraboo <paraboosanjay@gmail.com>
2017-09-22 16:42:40 -0400

    Last edit for LabA

--- src/LabA/LabA4.java
+++ src/LabA/LabA4.java
@@ -12,13 +12,13 @@ public class LabA4 {
         System.out.println("One Shift:\nOperation\tBinary\tDecimal");
         System.out.println("x = \t\t" + Integer.toBinaryString(x) + "\t" + x);
         System.out.println("x << 1 = \t" + Integer.toBinaryString(x << 1) + "\t" + (x << 1));
-        System.out.println("x >>> 1 = \t" + Integer.toBinaryString(x >>> 1) + "\t" + (x >> 1));
+        System.out.println("x >>> 1 = \t" + Integer.toBinaryString(x >>> 1) + "\t" + (x >>> 1));
         System.out.println("x >> 1 = \t" + Integer.toBinaryString(x >> 1) + "\t" + (x >> 1));
 
         System.out.println("\n\nTwo Shifts:\nOperation\tBinary\tDecimal");
         System.out.println("x = \t\t" + Integer.toBinaryString(x) + "\t" + x);
-        System.out.println("x << 1 = \t" + Integer.toBinaryString(x << 1) + "\t" + (x << 1));
-        System.out.println("x >>> 1 = \t" + Integer.toBinaryString(x >>> 1) + "\t" + (x >> 1));
-        System.out.println("x >> 1 = \t" + Integer.toBinaryString(x >> 1) + "\t" + (x >> 1));
+        System.out.println("x << 2 = \t" + Integer.toBinaryString(x << 2) + "\t" + (x << 2));
+        System.out.println("x >>> 2 = \t" + Integer.toBinaryString(x >>> 2) + "\t" + (x >>> 2));
+        System.out.println("x >> 2 = \t" + Integer.toBinaryString(x >> 2) + "\t" + (x >> 2));
     }
 }
